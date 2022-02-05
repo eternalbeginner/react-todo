7e9665f71b47d1d47c73503ca7f37f3282954d3e
eternalbeginner <ini.dwiii@gmail.com>
2022-02-05 19:10:48 +0800

    Implementing HOC

---
+++ src/hoc/withToggler.jsx
@@ -0,0 +1,29 @@
+const { Component } = require('react');
+
+const withToggler = (WrappedComponent) => {
+  return class WithToggler extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = { isHidden: false };
+      this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+      this.setState((prevState) => ({ isHidden: !prevState.isHidden }));
+    }
+
+    render() {
+      const { children } = this.props;
+      const { isHidden } = this.state;
+
+      return isHidden ? (
+        children(this.toggle)
+      ) : (
+        <WrappedComponent {...this.props} toggle={this.toggle} />
+      );
+    }
+  };
+};
+
+export default withToggler;
